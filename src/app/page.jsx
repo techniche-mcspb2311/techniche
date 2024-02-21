5eb4e80e48e94fb3b84354486388c759bf078622
Athan Clark <athan.clark@gmail.com>
2024-02-21 16:51:42 -0500

    mui with next.js

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function Home() {
+    return (
+        <Box sx={{ width: '100%', maxWidth: 500 }}>
+            <Typography variant="h1" gutterBottom>
+                h1. Heading
+            </Typography>
+            <Button>"Check 1 2"</Button>
+        </Box>
+  );
+}
