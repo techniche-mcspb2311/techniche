5d8b682f0f28957c6ba464fdb28154d28749ca96
stephx202 <stephhernandez202@gmail.com>
2024-02-28 15:10:34 -0800

    modal pops up when button clicked

--- src/app/dashboard/EditAccount.jsx
+++ src/app/dashboard/EditAccount.jsx
@@ -7,6 +7,10 @@ import Box from '@mui/material/Box';
 
 const EditAccount = () => {
   const [open, setOpen] = useState(false);
+  const [jobTitle, setJobTitle] = useState('Job Title');
+  const [city, setCity] = useState('City');
+  const [phoneNumber, setPhoneNumber] = useState('Phone Number');
+  const [imageUrl, setImageUrl] = useState('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png');
 
   const handleOpen = () => {
     setOpen(true);
@@ -16,21 +20,50 @@ const EditAccount = () => {
     setOpen(false);
   };
 
+  const handleSave = () => {
+    // Save changes (you can update your database or perform other actions here)
+    handleClose();
+  };
+
   return (
     <div>
       <Button variant="contained" color="primary" onClick={handleOpen}>
-        profile pic
+        Edit Profile
       </Button>
       <Modal open={open} onClose={handleClose}>
-        <div>
+        <Box>
           <h2>Edit Account</h2>
-          <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="profile pic" width="100" height="100"/>
-          <p>Job title</p>
-          <p>City</p>
-          <p>Phone number</p>
-          <p>Email</p>
-        </div>
-
+          <input
+            type="text"
+            value= {jobTitle}
+            onChange={(e) => setJobTitle(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+          <div style={{ width: '100%', textAlign: 'center' }}>
+            <img src={imageUrl} alt="profile pic" style={{ maxWidth: '10%', height: 'auto' }} />
+          </div>
+          <br />
+          <Button variant="contained" color="primary" onClick={handleSave}>
+            Save
+          </Button>
+        </Box>
       </Modal>
     </div>
   );
