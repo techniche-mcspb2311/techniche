32a86d66d399287a20847f8b6f898a6f13f8a21b
Heather Swinn <h.swinn@Heathers-MacBook-Air.local>
2024-02-29 12:24:33 -0800

    fixed new changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,17 +2,14 @@ import Image from "next/image";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { ChallengeProvider } from './context/ChallengeContext.js';
 
 export default function Home() {
     return (
-        <ChallengeProvider>
             <Box sx={{ width: '100%', maxWidth: 500 }}>
                 <Typography variant="h1" gutterBottom>
                     h1. Heading
                 </Typography>
                 <Button>Check 1 2</Button>
             </Box>
-        </ChallengeProvider>
   );
 }
