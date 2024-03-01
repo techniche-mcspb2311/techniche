d04e12699299ddbbc2ffff5b375e58591f89d35e
1337estofthe1337 <59178218+1337estofthe1337@users.noreply.github.com>
2024-03-01 12:45:11 -0700

    completed FAQ outline

--- src/app/landing/MissionStatement.jsx
+++ src/app/landing/MissionStatement.jsx
@@ -14,13 +14,13 @@ export default function MissionStatement() {
             </Box>
             <Box sx={{ p: 1 }}>
                 <Typography variant='body1' color='textSecondary'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
-                    sed do eiusmod tempor incididunt ut labore et dolore magna\
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation\
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis\
-                    aute irure dolor in reprehenderit in voluptate velit esse\
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat\
-                    cupidatat non proident, sunt in culpa qui officia deserunt\
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                    aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt
                     mollit anim id est laborum.
                 </Typography>
             </Box>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,13 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import MissionStatement from './landing/MissionStatement.jsx';
-
+import { EmployeeFAQ, CandidateFAQ } from "./landing/FAQ.jsx";
 export default function Home() {
     return (
         <Box>
             <MissionStatement />
+            <EmployeeFAQ />
+            <CandidateFAQ />
         </Box>
   );
 }
