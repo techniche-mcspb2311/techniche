6a21eb0a96ddcbd50d61f38f72db44cd539e81a5
1337estofthe1337 <59178218+1337estofthe1337@users.noreply.github.com>
2024-03-06 15:24:51 -0700

    made ColorMode.jsx file for light/dark mode

--- src/app/theme/ColorMode.jsx
+++ src/app/theme/ColorMode.jsx
@@ -3,4 +3,24 @@ import { createTheme } from '@mui/material/styles';
 
 const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
-export const useColorMode = () =>
+export const useColorMode = () => {
+  const [mode, setMode] = useState('light');
+
+  const toggleColorMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return { toggleColorMode, theme };
+};
+
+export const useColorModeContext = () => useContext(ColorModeContext);
